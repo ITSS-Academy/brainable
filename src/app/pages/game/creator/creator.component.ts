1764dfe7ad2972912ace36e9cefb4fcadf14e9d8
hhaoz <hao.tt03643@sinhvien.hoasen.edu.vn>
2024-08-07 00:27:34 +0700

    update layout creator

--- src/app/pages/game/creator/creator.component.ts
+++ src/app/pages/game/creator/creator.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-creator',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatToolbarModule],
   templateUrl: './creator.component.html',
   styleUrl: './creator.component.scss'
 })
 export class CreatorComponent {
-
 }
