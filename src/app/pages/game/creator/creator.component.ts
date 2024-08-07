214c9a58b1dbf4425f7b6143e6231ac176784722
hhaoz <hao.tt03643@sinhvien.hoasen.edu.vn>
2024-08-07 09:38:32 +0700

    add answers

--- src/app/pages/game/creator/creator.component.ts
+++ src/app/pages/game/creator/creator.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {MaterialModule} from "../../../shared/material.module";
 
 @Component({
   selector: 'app-creator',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatToolbarModule],
+  imports: [MaterialModule],
   templateUrl: './creator.component.html',
   styleUrl: './creator.component.scss'
 })
