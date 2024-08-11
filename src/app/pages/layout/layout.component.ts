796dd3f0d47b0ba6e996ffe4574e90b50c48136e
mtri <TRI.VM06567@sinhvien.hoasen.edu.vn>
2024-08-11 21:30:58 +0700

    resposive slide bar

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MaterialModule } from '../../shared/modules/material.module';
+import { fakeAsync } from '@angular/core/testing';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, SlidebarComponent, HeaderComponent, MaterialModule],
+  imports: [
+    RouterOutlet,
+    SlidebarComponent,
+    HeaderComponent,
+    MaterialModule,
+    NgClass,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  isSlideBarVisible = false;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const slideBar = document.querySelector('.slide-bar');
+    if (slideBar && !slideBar.contains(event.target as Node)) {
+      this.isSlideBarVisible = !this.isSlideBarVisible;
+    }
+  }
+
+  onMenuClick(): void {
+    this.isSlideBarVisible = false;
+  }
+}
