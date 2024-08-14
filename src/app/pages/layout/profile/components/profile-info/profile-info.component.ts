236490f41da514a886536d3c59d7d1db7e39fc1e
huyhoang <hoang.nh02147@sinhvien.hoasen.edu.vn>
2024-08-14 10:59:41 +0700

    update profile page

---
+++ src/app/pages/layout/profile/components/profile-info/profile-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-info.component.html',
+  styleUrl: './profile-info.component.scss'
+})
+export class ProfileInfoComponent {
+
+}
