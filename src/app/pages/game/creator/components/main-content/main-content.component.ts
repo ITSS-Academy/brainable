4086cf293b72975f2eac76203419716e42e4bcfc
mtri <TRI.VM06567@sinhvien.hoasen.edu.vn>
2024-08-16 06:42:59 +0700

    update question input in creator page

--- src/app/pages/game/creator/components/main-content/main-content.component.ts
+++ src/app/pages/game/creator/components/main-content/main-content.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import {MatIcon} from "@angular/material/icon";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SharedModule } from '../../../../../shared/modules/shared.module';
+import { MaterialModule } from '../../../../../shared/modules/material.module';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-    imports: [
-        MatIcon
-    ],
+  imports: [MaterialModule, SharedModule],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
 export class MainContentComponent {
+  question: string = '';
+  charCount: number = 0;
 
+  updateCharCount(): void {
+    this.charCount = 120 - this.question.length;
+  }
 }
