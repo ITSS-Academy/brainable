2d866db35aec20c899f9eaabb2d2f7bce90ca7dc
hhaoz <hao.tt03643@sinhvien.hoasen.edu.vn>
2024-08-16 22:25:32 +0700

    update question input

--- src/app/pages/game/creator/components/main-content/main-content.component.ts
+++ src/app/pages/game/creator/components/main-content/main-content.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-    imports: [
-        MatIcon
-    ],
+  imports: [
+    MatIcon,
+    FormsModule
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
